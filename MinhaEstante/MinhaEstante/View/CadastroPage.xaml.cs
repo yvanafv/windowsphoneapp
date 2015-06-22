150b4d74a22a721f1ca77eda724e8b6f7854487e
Franciello <francielllofigueiredo@gmail.com>
2015-06-22 20:57:49 -0300

    Adicionando placeholders nos inputs

--- MinhaEstante/MinhaEstante/View/CadastroPage.xaml.cs
+++ MinhaEstante/MinhaEstante/View/CadastroPage.xaml.cs
@@ -106,19 +106,6 @@ namespace MinhaEstante.View
 
         #region NavigationHelper registration
 
-        /// <summary>
-        /// The methods provided in this section are simply used to allow
-        /// NavigationHelper to respond to the page's navigation methods.
-        /// <para>
-        /// Page specific logic should be placed in event handlers for the  
-        /// <see cref="NavigationHelper.LoadState"/>
-        /// and <see cref="NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method 
-        /// in addition to page state preserved during an earlier session.
-        /// </para>
-        /// </summary>
-        /// <param name="e">Provides data for navigation methods and event
-        /// handlers that cannot cancel the navigation request.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             this.navigationHelper.OnNavigatedTo(e);
